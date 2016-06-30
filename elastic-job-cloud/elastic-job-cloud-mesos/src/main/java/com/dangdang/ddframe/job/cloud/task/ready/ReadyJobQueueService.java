3a92847d60e3d284b460d839e57be37656e6c8b6
terrymanu <terrymanu@163.com>
2016-06-30 17:25:36 +0800

    misfire

--- elastic-job-cloud/elastic-job-cloud-mesos/src/main/java/com/dangdang/ddframe/job/cloud/task/ready/ReadyJobQueueService.java
+++ elastic-job-cloud/elastic-job-cloud-mesos/src/main/java/com/dangdang/ddframe/job/cloud/task/ready/ReadyJobQueueService.java
@@ -17,6 +17,8 @@
 
 package com.dangdang.ddframe.job.cloud.task.ready;
 
+import com.dangdang.ddframe.job.cloud.job.config.CloudJobConfiguration;
+import com.dangdang.ddframe.job.cloud.job.config.ConfigurationService;
 import com.dangdang.ddframe.job.cloud.job.state.StateService;
 import com.dangdang.ddframe.reg.base.CoordinatorRegistryCenter;
 import com.google.common.base.Optional;
@@ -32,10 +34,13 @@ public class ReadyJobQueueService {
     
     private final CoordinatorRegistryCenter registryCenter;
     
+    private final ConfigurationService configService;
+    
     private final StateService stateService;
     
     public ReadyJobQueueService(final CoordinatorRegistryCenter registryCenter) {
         this.registryCenter = registryCenter;
+        configService = new ConfigurationService(registryCenter);
         stateService = new StateService(registryCenter);
     }
     
@@ -60,9 +65,17 @@ public class ReadyJobQueueService {
         List<String> jobNames = registryCenter.getChildrenKeys(ReadyJobQueueNode.ROOT);
         for (String each : jobNames) {
             String jobName = getLogicNodeForSequential(each);
+            Optional<CloudJobConfiguration> jobConfig = configService.load(jobName);
+            if (!jobConfig.isPresent()) {
+                registryCenter.remove(ReadyJobQueueNode.getReadyJobNodePath(each));
+                break;
+            }
             if (!stateService.isRunning(jobName)) {
                 registryCenter.remove(ReadyJobQueueNode.getReadyJobNodePath(each));
                 return Optional.of(jobName);
+            }
+            if (!jobConfig.get().isMisfire()) {
+                registryCenter.remove(ReadyJobQueueNode.getReadyJobNodePath(each));
             }
         }
         return Optional.absent();
