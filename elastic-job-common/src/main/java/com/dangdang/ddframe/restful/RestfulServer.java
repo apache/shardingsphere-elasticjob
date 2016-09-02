8c3144c5992a3292e970979e5796dedd70890a76
terrymanu <terrymanu@163.com>
2016-09-02 19:12:15 +0800

    refactor

--- elastic-job-common/src/main/java/com/dangdang/ddframe/restful/RestfulServer.java
+++ elastic-job-common/src/main/java/com/dangdang/ddframe/restful/RestfulServer.java
@@ -53,8 +53,8 @@ public final class RestfulServer {
     
     private ServletHolder getServletHolder(final String packages) {
         ServletHolder result = new ServletHolder(ServletContainer.class);
+        result.setInitParameter(PackagesResourceConfig.PROPERTY_PACKAGES, Joiner.on(",").join(RestfulServer.class.getPackage().getName(), packages));
         result.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", PackagesResourceConfig.class.getName());
-        result.setInitParameter("com.sun.jersey.config.property.packages", Joiner.on(",").join(RestfulServer.class.getPackage().getName(), packages));
         result.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", Boolean.TRUE.toString());
         result.setInitParameter("resteasy.scan.providers", Boolean.TRUE.toString());
         result.setInitParameter("resteasy.use.builtin.providers", Boolean.FALSE.toString());
