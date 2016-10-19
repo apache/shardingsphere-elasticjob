36d72c7cad12e4aa21063494a6f99b1c2977e9f4
terrymanu <terrymanu@163.com>
2016-10-19 20:17:19 +0800

    refactor

--- elastic-job-core/src/main/java/com/dangdang/ddframe/job/reg/zookeeper/ZookeeperRegistryCenter.java
+++ elastic-job-core/src/main/java/com/dangdang/ddframe/job/reg/zookeeper/ZookeeperRegistryCenter.java
@@ -42,8 +42,13 @@ import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
