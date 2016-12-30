e3dcdc84ec6c444173405aab2002aa13e6029c90
haocao <caohao@vip.qq.com>
2016-12-30 19:19:11 +0800

    Fixed #204.

--- elastic-job-common/elastic-job-common-core/src/main/java/com/dangdang/ddframe/job/event/type/JobExecutionEvent.java
+++ elastic-job-common/elastic-job-common-core/src/main/java/com/dangdang/ddframe/job/event/type/JobExecutionEvent.java
@@ -20,10 +20,10 @@ package com.dangdang.ddframe.job.event.type;
 import com.dangdang.ddframe.job.event.JobEvent;
 import com.dangdang.ddframe.job.exception.ExceptionUtil;
 import com.dangdang.ddframe.job.util.env.LocalHostService;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.UUID;
@@ -56,33 +56,43 @@ public class JobExecutionEvent implements JobEvent {
     
     private Date startTime = new Date();
     
+    @Setter
     private Date completeTime;
     
+    @Setter
     private boolean success;
     
+    @Setter
     private JobExecutionEventThrowable failureCause;
     
     /**
      * 作业执行成功.
+     * 
+     * @return 作业执行事件
      */
-    public void executionSuccess() {
-        completeTime = new Date();
-        success = true;
+    public JobExecutionEvent executionSuccess() {
+        JobExecutionEvent result = new JobExecutionEvent(id, hostname, ip, taskId, jobName, source, shardingItem, startTime, completeTime, success, failureCause);
+        result.setCompleteTime(new Date());
+        result.setSuccess(true);
+        return result;
     }
     
     /**
      * 作业执行失败.
      * 
      * @param failureCause 失败原因
+     * @return 作业执行事件
      */
-    public void executionFailure(final Throwable failureCause) {
-        completeTime = new Date();
-        success = false;
-        this.failureCause = new JobExecutionEventThrowable(failureCause);
+    public JobExecutionEvent executionFailure(final Throwable failureCause) {
+        JobExecutionEvent result = new JobExecutionEvent(id, hostname, ip, taskId, jobName, source, shardingItem, startTime, completeTime, success, new JobExecutionEventThrowable(failureCause));
+        result.setCompleteTime(new Date());
+        result.setSuccess(false);
+        return result;
     }
     
     /**
      * 获取失败原因.
+     * 
      * @return 失败原因
      */
     public String getFailureCause() {
