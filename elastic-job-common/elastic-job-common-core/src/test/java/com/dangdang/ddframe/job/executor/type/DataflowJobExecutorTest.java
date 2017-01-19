5abdf85345f67c61d6e01f1061885871b275603f
shijw823 <shijw823@163.com>
2017-01-19 16:47:29 +0800

    DataflowJob构造函数添加参数，代码测试覆盖

--- elastic-job-common/elastic-job-common-core/src/test/java/com/dangdang/ddframe/job/executor/type/DataflowJobExecutorTest.java
+++ elastic-job-common/elastic-job-common-core/src/test/java/com/dangdang/ddframe/job/executor/type/DataflowJobExecutorTest.java
@@ -165,7 +165,19 @@ public final class DataflowJobExecutorTest {
         verify(jobCaller).processData(3);
         verify(jobCaller).processData(4);
     }
-    
+
+    @Test
+    public void assertExecuteWhenFetchDataIsNotEmptyAndMultiThreadProcessDataForStreamingProcess() {
+        setUp(true, 2, ShardingContextsBuilder.getMultipleShardingContexts());
+        when(jobCaller.fetchData(0)).thenReturn(Arrays.<Object>asList(1, 2, 3, 4));
+        dataflowJobExecutor.execute();
+        verify(jobCaller).fetchData(0);
+        verify(jobCaller).processData(1);
+        verify(jobCaller).processData(2);
+        verify(jobCaller).processData(3);
+        verify(jobCaller).processData(4);
+    }
+
     private void setUp(final boolean isStreamingProcess, final int processDataThreadCount, final ShardingContexts shardingContexts) {
         this.shardingContexts = shardingContexts;
         when(jobFacade.loadJobRootConfiguration(true)).thenReturn(new TestDataflowJobConfiguration(isStreamingProcess, processDataThreadCount));
