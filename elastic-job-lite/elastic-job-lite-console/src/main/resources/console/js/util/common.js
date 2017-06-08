d5d4c7c284ef2b36f982f5e815710f47c5a2f7e2
Zhangxinguo <1245579343@qq.com>
2017-06-08 18:40:51 +0800

    doLocale

--- elastic-job-lite/elastic-job-lite-console/src/main/resources/console/js/util/common.js
+++ elastic-job-lite/elastic-job-lite-console/src/main/resources/console/js/util/common.js
@@ -11,11 +11,7 @@ function showDialog(msg, timeout) {
 }
 
 function showSuccessDialog() {
-    if ($("#content").hasClass("lang-en")) {
-        showInfoDialog("Operation complete successfully");
-    } else {
-        showInfoDialog("操作已成功完成");
-    }
+    showInfoDialog($.i18n.prop("operation-succeed"));
 }
 
 function showInfoDialog(msg) {
@@ -45,62 +41,34 @@ function authorityControl() {
 }
 
 function showDeleteConfirmModal() {
-    if ($("#content").hasClass("lang-en")) {
-        $("#confirm-info").text("Are you sure to delete it？");
-    } else {
-        $("#confirm-info").text("确认要删除吗？");
-    }
+    $("#confirm-info").text($.i18n.prop("confirm-to-delete"));
     $("#confirm-dialog").modal({backdrop: 'static', keyboard: true});
 }
 
 function showShutdownConfirmModal() {
-     if ($("#content").hasClass("lang-en")) {
-         $("#confirm-info").text("Are you sure to close it？");
-     } else {
-         $("#confirm-info").text("确认要关闭吗？");
-     }
+    $("#confirm-info").text($.i18n.prop("confirm-to-close"));
     $("#confirm-dialog").modal({backdrop: 'static', keyboard: true});
 }
 
 function showUpdateConfirmModal() {
-     if ($("#content").hasClass("lang-en")) {
-         $("#confirm-info").text("It will affect the running job by updating these fields are Monitor Execution、Failover and Misfire，please be careful!");
-     } else {
-         $("#confirm-info").text("更新监控作业执行时状态、支持自动失效转移、支持misfire会对运行中的作业造成影响，请慎重操作！");
-     }
+    $("#confirm-info").text($.i18n.prop("update-job-confirm-info"));
     $("#confirm-dialog").modal({backdrop: 'static', keyboard: true});
 }
 
 function showDataSourceFailureDialog() {
-    if ($("#content").hasClass("lang-en")) {
-        showFailureDialog("The operation is unsuccessful, for the reason: the connection failed, please check the event trace data source configuration");
-    } else {
-        showFailureDialog("操作未成功，原因：连接失败，请检查事件追踪数据源配置");
-    }
+    showFailureDialog($.i18n.prop("dataSource-connect-failed"));
 }
 
 function showRegCenterFailureDialog() {
-    if ($("#content").hasClass("lang-en")) {
-        showFailureDialog("The operation is unsuccessful, for the reason: the connection failed, please check the registry center configuration");
-    } else {
-        showFailureDialog("操作未成功，原因：连接失败，请检查注册中心配置");
-    }
+    showFailureDialog($.i18n.prop("regCenter-connect-failed"));
 }
 
 function showDataSourceTestConnectionSuccessDialog() {
-    if ($("#content").hasClass("lang-en")) {
-        showInfoDialog("Event trace data source test connection success!");
-    } else {
-        showInfoDialog("事件追踪数据源测试连接成功!");
-    }
+    showInfoDialog($.i18n.prop("dataSource-test-succeed"));
 }
 
 function showDataSourceTestConnectionFailureDialog() {
-    if ($("#content").hasClass("lang-en")) {
-        showInfoDialog("Event trace data source test connection failure!");
-    } else {
-        showInfoDialog("事件追踪数据源测试连接失败!");
-    }
+    showInfoDialog($.i18n.prop("dataSource-test-fail"));
 }
 
 function i18n(lang) {
