06a28e090be56ce45d32690161fe4faae6d43017
xiaobaoger <392601369@qq.com>
2017-12-16 21:19:11 +0800

    Update JobSchedulerTest.java
    添加assertInitForClassLoader单元测试

--- elastic-job-lite/elastic-job-lite-core/src/test/java/com/dangdang/ddframe/job/lite/api/JobSchedulerTest.java
+++ elastic-job-lite/elastic-job-lite-core/src/test/java/com/dangdang/ddframe/job/lite/api/JobSchedulerTest.java
@@ -78,4 +78,15 @@ public final class JobSchedulerTest {
         assertThat(scheduler.getListenerManager().getTriggerListeners().get(0), instanceOf(JobTriggerListener.class));
         assertTrue(scheduler.isStarted());
     }
+	
+	@Test
+    public void assertInitForClassLoader() throws NoSuchFieldException, SchedulerException {
+        when(schedulerFacade.updateJobConfiguration(liteJobConfig)).thenReturn(liteJobConfig);
+        when(schedulerFacade.newJobTriggerListener()).thenReturn(new JobTriggerListener(null, null));
+        jobScheduler.init(JobSchedulerTest.class.getClassLoader());
+        verify(schedulerFacade).registerStartUpInfo(true);
+        Scheduler scheduler = ReflectionUtils.getFieldValue(JobRegistry.getInstance().getJobScheduleController("test_job"), JobScheduleController.class.getDeclaredField("scheduler"));
+        assertThat(scheduler.getListenerManager().getTriggerListeners().get(0), instanceOf(JobTriggerListener.class));
+        assertTrue(scheduler.isStarted());
+    }
 }
