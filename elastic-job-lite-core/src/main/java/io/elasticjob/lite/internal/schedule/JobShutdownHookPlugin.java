2b4dd35b827a019beb9c6ede54e15b11385ef241
Leonidasz <leonshow@foxmail.com>
2019-12-19 20:17:10 +0800

    Do not add the Quartz Shutdown-Hook while JobShutdownHookPlugin execute initialize.

--- elastic-job-lite-core/src/main/java/io/elasticjob/lite/internal/schedule/JobShutdownHookPlugin.java
+++ elastic-job-lite-core/src/main/java/io/elasticjob/lite/internal/schedule/JobShutdownHookPlugin.java
@@ -17,27 +17,8 @@ public final class JobShutdownHookPlugin extends ShutdownHookPlugin {
     
     private String jobName;
     
-    private Thread t;
-    
     @Override
     public void initialize(final String name, final Scheduler scheduler, final ClassLoadHelper classLoadHelper) throws SchedulerException {
-        getLog().info("Registering Quartz shutdown hook.");
-    
-        t = new Thread("Quartz Shutdown-Hook "
-                + scheduler.getSchedulerName()) {
-            @Override
-            public void run() {
-                getLog().info("Shutting down Quartz...");
-                try {
-                    scheduler.shutdown(isCleanShutdown());
-                } catch (SchedulerException e) {
-                    getLog().info(
-                            "Error shutting down Quartz: " + e.getMessage(), e);
-                }
-            }
-        };
-    
-        Runtime.getRuntime().addShutdownHook(t);
         jobName = scheduler.getSchedulerName();
     }
     
@@ -52,8 +33,5 @@ public final class JobShutdownHookPlugin extends ShutdownHookPlugin {
             leaderService.removeLeader();
         }
         new InstanceService(regCenter, jobName).removeInstance();
-        if (!Thread.currentThread().getName().equals(t.getName())) {
-            Runtime.getRuntime().removeShutdownHook(t);
-        }
     }
 }
