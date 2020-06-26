120e8fcf31b4fad87f4dafffb5621baf40895b1a
terrymanu <terrymanu@163.com>
2020-06-27 00:33:07 +0800

    Refactor LiteJobConfigurationGsonTypeAdapter.read

--- elastic-job-lite-core/src/main/java/org/apache/shardingsphere/elasticjob/lite/internal/config/json/LiteJobConfigurationGsonTypeAdapter.java
+++ elastic-job-lite-core/src/main/java/org/apache/shardingsphere/elasticjob/lite/internal/config/json/LiteJobConfigurationGsonTypeAdapter.java
@@ -33,6 +33,7 @@ import org.apache.shardingsphere.elasticjob.lite.config.simple.SimpleJobConfigur
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Job configuration gson type adapter.
@@ -41,99 +42,42 @@ public final class LiteJobConfigurationGsonTypeAdapter extends TypeAdapter<LiteJ
     
     @Override
     public LiteJobConfiguration read(final JsonReader in) throws IOException {
-        String jobName = "";
-        String cron = "";
-        int shardingTotalCount = 0;
-        String shardingItemParameters = "";
-        String jobParameter = "";
-        boolean failover = false;
-        boolean misfire = failover;
-        String jobExecutorServiceHandlerType = "";
-        String jobErrorHandlerType = "";
-        String description = "";
-        JobType jobType = null;
-        boolean streamingProcess = false;
-        String scriptCommandLine = "";
-        Map<String, Object> customizedValueMap = new HashMap<>(32, 1);
+        Map<String, Object> jsonValueMap = new HashMap<>(32, 1);
         in.beginObject();
         while (in.hasNext()) {
             String jsonName = in.nextName();
-            switch (jsonName) {
-                case "jobName":
-                    jobName = in.nextString();
-                    break;
-                case "cron":
-                    cron = in.nextString();
-                    break;
-                case "shardingTotalCount":
-                    shardingTotalCount = in.nextInt();
-                    break;
-                case "shardingItemParameters":
-                    shardingItemParameters = in.nextString();
-                    break;
-                case "jobParameter":
-                    jobParameter = in.nextString();
-                    break;
-                case "failover":
-                    failover = in.nextBoolean();
-                    break;
-                case "misfire":
-                    misfire = in.nextBoolean();
-                    break;
-                case "jobExecutorServiceHandlerType":
-                    jobExecutorServiceHandlerType = in.nextString();
-                    break;
-                case "jobErrorHandlerType":
-                    jobErrorHandlerType = in.nextString();
-                    break;
-                case "description":
-                    description = in.nextString();
-                    break;
-                case "jobType":
-                    jobType = JobType.valueOf(in.nextString());
-                    break;
-                case "streamingProcess":
-                    streamingProcess = in.nextBoolean();
-                    break;
-                case "scriptCommandLine":
-                    scriptCommandLine = in.nextString();
-                    break;
-                case LiteJobConfigurationConstants.MONITOR_EXECUTION:
-                    customizedValueMap.put(jsonName, in.nextBoolean());
-                    break;
-                case LiteJobConfigurationConstants.MAX_TIME_DIFF_SECONDS:
-                    customizedValueMap.put(jsonName, in.nextInt());
-                    break;
-                case LiteJobConfigurationConstants.MONITOR_PORT:
-                    customizedValueMap.put(jsonName, in.nextInt());
-                    break;
-                case LiteJobConfigurationConstants.JOB_SHARDING_STRATEGY_TYPE:
-                    customizedValueMap.put(jsonName, in.nextString());
-                    break;
-                case LiteJobConfigurationConstants.RECONCILE_INTERVAL_MINUTES:
-                    customizedValueMap.put(jsonName, in.nextInt());
-                    break;
-                case LiteJobConfigurationConstants.DISABLED:
-                    customizedValueMap.put(jsonName, in.nextBoolean());
-                    break;
-                case LiteJobConfigurationConstants.OVERWRITE:
-                    customizedValueMap.put(jsonName, in.nextBoolean());
-                    break;
-                default:
-                    in.skipValue();
-                    break;
+            Optional<JobConfigurationJsonEnum> jsonEnum = JobConfigurationJsonEnum.find(jsonName);
+            if (jsonEnum.isPresent()) {
+                Class<?> jsonType = jsonEnum.get().getJsonType();
+                if (boolean.class == jsonType) {
+                    jsonValueMap.put(jsonName, in.nextBoolean());
+                } else if (int.class == jsonType) {
+                    jsonValueMap.put(jsonName, in.nextInt());
+                } else if (String.class == jsonType) {
+                    jsonValueMap.put(jsonName, in.nextString());
+                }
+            } else {
+                in.skipValue();
             }
         }
         in.endObject();
-        JobCoreConfiguration coreConfig = getJobCoreConfiguration(
-                jobName, cron, shardingTotalCount, shardingItemParameters, jobParameter, failover, misfire, jobExecutorServiceHandlerType, jobErrorHandlerType, description);
-        JobTypeConfiguration typeConfig = getJobTypeConfiguration(coreConfig, jobType, streamingProcess, scriptCommandLine);
-        return getJobRootConfiguration(typeConfig, customizedValueMap);
+        JobCoreConfiguration coreConfig = createJobCoreConfiguration((String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_NAME.getJsonName()), 
+                (String) jsonValueMap.get(JobConfigurationJsonEnum.CRON.getJsonName()), (int) jsonValueMap.getOrDefault(JobConfigurationJsonEnum.SHARDING_TOTAL_COUNT.getJsonName(), 0), 
+                (String) jsonValueMap.get(JobConfigurationJsonEnum.SHARDING_ITEM_PARAMETERS.getJsonName()), (String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_PARAMETER.getJsonName()), 
+                (boolean) jsonValueMap.getOrDefault(JobConfigurationJsonEnum.FAILOVER.getJsonName(), false), 
+                (boolean) jsonValueMap.getOrDefault(JobConfigurationJsonEnum.MISFIRE.getJsonName(), false), 
+                (String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_EXECUTOR_SERVICE_HANDLER_TYPE.getJsonName()), 
+                (String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_ERROR_HANDLER_TYPE.getJsonName()), 
+                (String) jsonValueMap.get(JobConfigurationJsonEnum.DESCRIPTION.getJsonName()));
+        JobTypeConfiguration typeConfig = createJobTypeConfiguration(coreConfig, JobType.valueOf((String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_TYPE.getJsonName())), 
+                (boolean) jsonValueMap.getOrDefault(JobConfigurationJsonEnum.STREAMING_PROCESS.getJsonName(), false), 
+                (String) jsonValueMap.getOrDefault(JobConfigurationJsonEnum.SCRIPT_COMMAND_LINE.getJsonName(), ""));
+        return createJobRootConfiguration(typeConfig, jsonValueMap);
     }
     
-    private JobCoreConfiguration getJobCoreConfiguration(final String jobName, final String cron, final int shardingTotalCount,
-                                                         final String shardingItemParameters, final String jobParameter, final boolean failover,
-                                                         final boolean misfire, final String jobExecutorServiceHandlerType, final String jobErrorHandlerType, final String description) {
+    private JobCoreConfiguration createJobCoreConfiguration(final String jobName, final String cron, final int shardingTotalCount,
+                                                            final String shardingItemParameters, final String jobParameter, final boolean failover,
+                                                            final boolean misfire, final String jobExecutorServiceHandlerType, final String jobErrorHandlerType, final String description) {
         return JobCoreConfiguration.newBuilder(jobName, cron, shardingTotalCount)
                 .shardingItemParameters(shardingItemParameters).jobParameter(jobParameter).failover(failover).misfire(misfire).description(description)
                 .jobExecutorServiceHandlerType(jobExecutorServiceHandlerType)
@@ -141,7 +85,7 @@ public final class LiteJobConfigurationGsonTypeAdapter extends TypeAdapter<LiteJ
                 .build();
     }
     
-    private JobTypeConfiguration getJobTypeConfiguration(final JobCoreConfiguration coreConfig, final JobType jobType, final boolean streamingProcess, final String scriptCommandLine) {
+    private JobTypeConfiguration createJobTypeConfiguration(final JobCoreConfiguration coreConfig, final JobType jobType, final boolean streamingProcess, final String scriptCommandLine) {
         Preconditions.checkNotNull(jobType, "jobType cannot be null.");
         switch (jobType) {
             case SIMPLE:
@@ -155,28 +99,28 @@ public final class LiteJobConfigurationGsonTypeAdapter extends TypeAdapter<LiteJ
         }
     }
     
-    private LiteJobConfiguration getJobRootConfiguration(final JobTypeConfiguration typeConfig, final Map<String, Object> customizedValueMap) {
+    private LiteJobConfiguration createJobRootConfiguration(final JobTypeConfiguration typeConfig, final Map<String, Object> jsonValueMap) {
         LiteJobConfiguration.Builder builder = LiteJobConfiguration.newBuilder(typeConfig);
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.MONITOR_EXECUTION)) {
-            builder.monitorExecution((boolean) customizedValueMap.get(LiteJobConfigurationConstants.MONITOR_EXECUTION));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.MONITOR_EXECUTION.getJsonName())) {
+            builder.monitorExecution((boolean) jsonValueMap.get(JobConfigurationJsonEnum.MONITOR_EXECUTION.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.MAX_TIME_DIFF_SECONDS)) {
-            builder.maxTimeDiffSeconds((int) customizedValueMap.get(LiteJobConfigurationConstants.MAX_TIME_DIFF_SECONDS));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.MAX_TIME_DIFF_SECONDS.getJsonName())) {
+            builder.maxTimeDiffSeconds((int) jsonValueMap.get(JobConfigurationJsonEnum.MAX_TIME_DIFF_SECONDS.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.MONITOR_PORT)) {
-            builder.monitorPort((int) customizedValueMap.get(LiteJobConfigurationConstants.MONITOR_PORT));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.MONITOR_PORT.getJsonName())) {
+            builder.monitorPort((int) jsonValueMap.get(JobConfigurationJsonEnum.MONITOR_PORT.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.JOB_SHARDING_STRATEGY_TYPE)) {
-            builder.jobShardingStrategyType((String) customizedValueMap.get(LiteJobConfigurationConstants.JOB_SHARDING_STRATEGY_TYPE));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.JOB_SHARDING_STRATEGY_TYPE.getJsonName())) {
+            builder.jobShardingStrategyType((String) jsonValueMap.get(JobConfigurationJsonEnum.JOB_SHARDING_STRATEGY_TYPE.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.RECONCILE_INTERVAL_MINUTES)) {
-            builder.reconcileIntervalMinutes((int) customizedValueMap.get(LiteJobConfigurationConstants.RECONCILE_INTERVAL_MINUTES));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.RECONCILE_INTERVAL_MINUTES.getJsonName())) {
+            builder.reconcileIntervalMinutes((int) jsonValueMap.get(JobConfigurationJsonEnum.RECONCILE_INTERVAL_MINUTES.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.DISABLED)) {
-            builder.disabled((boolean) customizedValueMap.get(LiteJobConfigurationConstants.DISABLED));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.DISABLED.getJsonName())) {
+            builder.disabled((boolean) jsonValueMap.get(JobConfigurationJsonEnum.DISABLED.getJsonName()));
         }
-        if (customizedValueMap.containsKey(LiteJobConfigurationConstants.OVERWRITE)) {
-            builder.overwrite((boolean) customizedValueMap.get(LiteJobConfigurationConstants.OVERWRITE));
+        if (jsonValueMap.containsKey(JobConfigurationJsonEnum.OVERWRITE.getJsonName())) {
+            builder.overwrite((boolean) jsonValueMap.get(JobConfigurationJsonEnum.OVERWRITE.getJsonName()));
         }
         return builder.build();
     }
@@ -214,5 +158,4 @@ public final class LiteJobConfigurationGsonTypeAdapter extends TypeAdapter<LiteJ
         out.name(LiteJobConfigurationConstants.OVERWRITE).value(value.isOverwrite());
         out.endObject();
     }
-    
 }
