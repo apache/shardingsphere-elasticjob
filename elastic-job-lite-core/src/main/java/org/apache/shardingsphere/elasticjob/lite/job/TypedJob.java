1889c75aea4db10adc746c8980e6af066c8cdbb7
Liang Zhang <terrymanu@163.com>
2020-07-03 16:25:57 +0800

    Refactor TypedJob (#919)

--- elastic-job-lite-core/src/main/java/org/apache/shardingsphere/elasticjob/lite/job/TypedJob.java
+++ elastic-job-lite-core/src/main/java/org/apache/shardingsphere/elasticjob/lite/job/TypedJob.java
@@ -17,7 +17,7 @@
 
 package org.apache.shardingsphere.elasticjob.lite.job;
 
-import org.apache.shardingsphere.elasticjob.lite.api.type.simple.SimpleJob;
+import org.apache.shardingsphere.elasticjob.lite.api.ElasticJob;
 import org.apache.shardingsphere.elasticjob.lite.spi.TypedSPI;
 
 import java.util.Properties;
@@ -25,7 +25,7 @@ import java.util.Properties;
 /**
  * Typed job.
  */
-public interface TypedJob extends SimpleJob, TypedSPI {
+public interface TypedJob extends ElasticJob, TypedSPI {
     
     /**
      * Get props.
