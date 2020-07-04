2a76c9174fae371bcddee463d040797867f7d3aa
Kane Sun <kane.sun@outlook.com>
2020-07-04 14:32:36 +0800

    Remove: remove the checkstyle off in main method

--- examples/elastic-job-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/SpringBootMain.java
+++ examples/elastic-job-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/SpringBootMain.java
@@ -23,9 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMain {
     
-    // CHECKSTYLE:OFF
     public static void main(final String[] args) {
-    // CHECKSTYLE:ON
         EmbedZookeeperServer.start(6181);
         SpringApplication.run(SpringBootMain.class, args);
     }
