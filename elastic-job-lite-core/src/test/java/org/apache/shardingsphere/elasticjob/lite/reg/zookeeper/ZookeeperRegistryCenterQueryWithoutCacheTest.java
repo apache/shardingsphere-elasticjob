ae7574ed51d2c5804ff70e217fb26353f72189d9
terrymanu <terrymanu@163.com>
2020-07-05 13:57:07 +0800

    For code format

--- elastic-job-lite-core/src/test/java/org/apache/shardingsphere/elasticjob/lite/reg/zookeeper/ZookeeperRegistryCenterQueryWithoutCacheTest.java
+++ elastic-job-lite-core/src/test/java/org/apache/shardingsphere/elasticjob/lite/reg/zookeeper/ZookeeperRegistryCenterQueryWithoutCacheTest.java
@@ -90,7 +90,7 @@ public final class ZookeeperRegistryCenterQueryWithoutCacheTest {
         long updatedRegCenterTime = zkRegCenter.getRegistryCenterTime("/_systemTime/current");
         assertTrue(regCenterTime < updatedRegCenterTime);
     }
-
+    
     @Test
     public void assertGetWithoutNode() {
         assertNull(zkRegCenter.get("/notExisted"));
