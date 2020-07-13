4178846d04f137e2fad3a35de418fcbc8c94ffb7
terrymanu <terrymanu@163.com>
2020-07-13 13:22:39 +0800

    Refactor JobConfigurationContextTest

--- elasticjob-cloud/elasticjob-cloud-executor/src/test/java/org/apache/shardingsphere/elasticjob/cloud/executor/JobConfigurationContextTest.java
+++ elasticjob-cloud/elasticjob-cloud-executor/src/test/java/org/apache/shardingsphere/elasticjob/cloud/executor/JobConfigurationContextTest.java
@@ -17,9 +17,6 @@
 
 package org.apache.shardingsphere.elasticjob.cloud.executor;
 
-import org.apache.shardingsphere.elasticjob.cloud.api.JobType;
-import org.apache.shardingsphere.elasticjob.cloud.config.dataflow.DataflowJobConfiguration;
-import org.apache.shardingsphere.elasticjob.cloud.config.script.ScriptJobConfiguration;
 import org.apache.shardingsphere.elasticjob.cloud.config.simple.SimpleJobConfiguration;
 import org.apache.shardingsphere.elasticjob.cloud.executor.fixture.TestJob;
 import org.junit.Test;
@@ -36,22 +33,22 @@ public class JobConfigurationContextTest {
     
     @Test
     public void assertSimpleJobConfigurationContext() {
-        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap(JobType.SIMPLE)).getTypeConfig() instanceof SimpleJobConfiguration);
+        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap()).getTypeConfig() instanceof SimpleJobConfiguration);
     }
     
     @Test
     public void assertDataflowJobConfigurationContext() {
-        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap(JobType.DATAFLOW)).getTypeConfig() instanceof DataflowJobConfiguration);
+        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap()).getTypeConfig() instanceof SimpleJobConfiguration);
     }
     
     @Test
     public void assertScriptJobConfigurationContext() {
-        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap(JobType.SCRIPT)).getTypeConfig() instanceof ScriptJobConfiguration);
+        assertTrue(new JobConfigurationContext(buildJobConfigurationContextMap()).getTypeConfig() instanceof SimpleJobConfiguration);
     }
     
     @Test
     public void assertSpringSimpleJobConfigurationContext() {
-        Map<String, String> context = buildJobConfigurationContextMap(JobType.SIMPLE);
+        Map<String, String> context = buildJobConfigurationContextMap();
         context.put("beanName", "springSimpleJobName");
         context.put("applicationContext", "applicationContext.xml");
         assertThat(new JobConfigurationContext(context).getBeanName(), is("springSimpleJobName"));
@@ -60,22 +57,18 @@ public class JobConfigurationContextTest {
     
     @Test
     public void assertSimpleJobConfigurationContextWithExecutionType() {
-        Map<String, String> context = buildJobConfigurationContextMap(JobType.SIMPLE);
+        Map<String, String> context = buildJobConfigurationContextMap();
         assertTrue(new JobConfigurationContext(context).isTransient());
         context.put("cron", "0/1 * * * * ?");
         assertFalse(new JobConfigurationContext(context).isTransient());
     }
     
-    private Map<String, String> buildJobConfigurationContextMap(final JobType jobType) {
+    private Map<String, String> buildJobConfigurationContextMap() {
         Map<String, String> result = new HashMap<>();
         result.put("jobName", "configuration_map_job");
         result.put("jobClass", TestJob.class.getCanonicalName());
-        result.put("jobType", jobType.name());
-        if (jobType == JobType.DATAFLOW) {
-            result.put("streamingProcess", Boolean.TRUE.toString());
-        } else if (jobType == JobType.SCRIPT) {
-            result.put("scriptCommandLine", "echo test");
-        }
+        result.put("streamingProcess", Boolean.TRUE.toString());
+        result.put("scriptCommandLine", "echo test");
         return result;
     }
 }
