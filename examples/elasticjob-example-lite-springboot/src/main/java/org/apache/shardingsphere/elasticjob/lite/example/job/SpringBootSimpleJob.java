39c2e8c2e0d5964b621491a281e5ccfe463a5287
吴伟杰 <root496879118@me.com>
2020-07-19 16:33:02 +0800

    Update example for spring boot starter (#1164)
    - Show usage of OneOffJob.
    - Replace System.out with Logger.

--- examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/SpringBootSimpleJob.java
+++ examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/SpringBootSimpleJob.java
@@ -21,6 +21,8 @@ import org.apache.shardingsphere.elasticjob.api.ShardingContext;
 import org.apache.shardingsphere.elasticjob.lite.example.entity.Foo;
 import org.apache.shardingsphere.elasticjob.lite.example.repository.FooRepository;
 import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -31,13 +33,15 @@ import java.util.List;
 @Component
 public class SpringBootSimpleJob implements SimpleJob {
 
+    private final Logger logger = LoggerFactory.getLogger(SpringBootSimpleJob.class);
+
     @Autowired
     private FooRepository fooRepository;
 
     @Override
     public void execute(ShardingContext shardingContext) {
-        System.out.println(String.format("Item: %s | Time: %s | Thread: %s | %s",
-                shardingContext.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()), Thread.currentThread().getId(), "SIMPLE"));
+        logger.info("Item: {} | Time: {} | Thread: {} | {}",
+                shardingContext.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()), Thread.currentThread().getId(), "SIMPLE");
         List<Foo> data = fooRepository.findTodoData(shardingContext.getShardingParameter(), 10);
         for (Foo each : data) {
             fooRepository.setCompleted(each.getId());
