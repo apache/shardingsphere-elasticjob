5e2240e3ac9a106fa7b4e933cf27c1525b5a0a2e
terrymanu <terrymanu@163.com>
2020-07-20 19:47:16 +0800

    Add prod package

--- elasticjob-cloud/elasticjob-cloud-executor/src/main/java/org/apache/shardingsphere/elasticjob/cloud/executor/prod/JobTypeConfigurationUtil.java
+++ elasticjob-cloud/elasticjob-cloud-executor/src/main/java/org/apache/shardingsphere/elasticjob/cloud/executor/prod/JobTypeConfigurationUtil.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.elasticjob.cloud.executor;
+package org.apache.shardingsphere.elasticjob.cloud.executor.prod;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
