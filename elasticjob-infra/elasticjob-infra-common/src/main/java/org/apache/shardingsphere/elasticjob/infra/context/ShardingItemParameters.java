22b6f5b2b57b2b5e35e8c01a6e4922253aede84d
terrymanu <terrymanu@163.com>
2020-07-20 18:25:04 +0800

    Move ShardingItemParameters

--- elasticjob-infra/elasticjob-infra-common/src/main/java/org/apache/shardingsphere/elasticjob/infra/context/ShardingItemParameters.java
+++ elasticjob-infra/elasticjob-infra-common/src/main/java/org/apache/shardingsphere/elasticjob/infra/context/ShardingItemParameters.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.elasticjob.lite.internal.sharding;
+package org.apache.shardingsphere.elasticjob.infra.context;
 
 import com.google.common.base.Strings;
 import lombok.AllArgsConstructor;
