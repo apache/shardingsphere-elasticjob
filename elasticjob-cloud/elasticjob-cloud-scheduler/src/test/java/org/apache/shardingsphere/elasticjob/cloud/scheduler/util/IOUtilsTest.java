2e6ca896ea684f00a9b40bf183bc204bde1e2f29
keker(zhaoke) <asdzhaoke@163.com>
2020-07-22 21:45:55 +0800

    fix code bad taste.

--- elasticjob-cloud/elasticjob-cloud-scheduler/src/test/java/org/apache/shardingsphere/elasticjob/cloud/scheduler/util/IOUtilsTest.java
+++ elasticjob-cloud/elasticjob-cloud-scheduler/src/test/java/org/apache/shardingsphere/elasticjob/cloud/scheduler/util/IOUtilsTest.java
@@ -27,7 +27,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class IOUtilsTest {
+public final class IOUtilsTest {
     
     @Test
     public void assertToStringFromInputStream() throws Exception {
