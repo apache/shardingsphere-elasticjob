6b03630ad38b4ac2438b1b0ea842ffeaa8f41dc9
terrymanu <terrymanu@163.com>
2020-08-03 13:00:24 +0800

    Revise code style

--- examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/repository/FooRepository.java
+++ examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/repository/FooRepository.java
@@ -27,25 +27,25 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class FooRepository {
-
+    
     private final Map<Long, Foo> data = new ConcurrentHashMap<>(300, 1);
-
+    
     public FooRepository() {
         init();
     }
-
+    
     private void init() {
         addData(0L, 100L, "Beijing");
         addData(100L, 200L, "Shanghai");
         addData(200L, 300L, "Guangzhou");
     }
-
+    
     private void addData(final long idFrom, final long idTo, final String location) {
         for (long i = idFrom; i < idTo; i++) {
             data.put(i, new Foo(i, location, Foo.Status.TODO));
         }
     }
-
+    
     public List<Foo> findTodoData(final String location, final int limit) {
         List<Foo> result = new ArrayList<>(limit);
         int count = 0;
@@ -61,7 +61,7 @@ public class FooRepository {
         }
         return result;
     }
-
+    
     public void setCompleted(final long id) {
         data.get(id).setStatus(Foo.Status.COMPLETED);
     }
