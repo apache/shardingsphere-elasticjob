da09809c755c327bcb6bb8630a42e24b3ead615e
吴伟杰 <root496879118@me.com>
2020-10-19 21:06:06 +0800

    Remove unused field path in HandlerNotFoundException (#1609)

--- elasticjob-infra/elasticjob-restful/src/main/java/org/apache/shardingsphere/elasticjob/restful/handler/HandlerNotFoundException.java
+++ elasticjob-infra/elasticjob-restful/src/main/java/org/apache/shardingsphere/elasticjob/restful/handler/HandlerNotFoundException.java
@@ -23,10 +23,7 @@ public final class HandlerNotFoundException extends RuntimeException {
     
     private static final long serialVersionUID = 7316145545440327554L;
     
-    private final String path;
-    
     public HandlerNotFoundException(final String path) {
         super(MessageFormat.format("No handler found for [{0}].", path));
-        this.path = path;
     }
 }
