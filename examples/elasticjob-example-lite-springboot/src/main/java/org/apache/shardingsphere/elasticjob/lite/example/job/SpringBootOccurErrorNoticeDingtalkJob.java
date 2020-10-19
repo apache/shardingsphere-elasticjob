feeb58a867326dd81e16e8253124412d10a61c13
terrymanu <terrymanu@163.com>
2020-10-19 20:35:59 +0800

    Fix typo

--- examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/SpringBootOccurErrorNoticeDingtalkJob.java
+++ examples/elasticjob-example-lite-springboot/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/SpringBootOccurErrorNoticeDingtalkJob.java
@@ -22,10 +22,10 @@ import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBootOccurErrorNoticeDingtlakJob implements SimpleJob {
+public class SpringBootOccurErrorNoticeDingtalkJob implements SimpleJob {
     
     @Override
-    public void execute(ShardingContext shardingContext) {
+    public void execute(final ShardingContext shardingContext) {
         throw new RuntimeException(String.format("An exception has occurred in Job, The parameter is %s", shardingContext.getShardingParameter()));
     }
 }
