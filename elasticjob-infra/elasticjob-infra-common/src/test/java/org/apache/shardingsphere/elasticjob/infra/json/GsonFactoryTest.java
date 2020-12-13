388775031abfb1c9b006862b58cad75102b33249
Luke Yi <lukeyi@pku.edu.cn>
2020-12-13 19:22:18 +0800

    Update GsonFactoryTest.java
    Remove potential test order dependency

--- elasticjob-infra/elasticjob-infra-common/src/test/java/org/apache/shardingsphere/elasticjob/infra/json/GsonFactoryTest.java
+++ elasticjob-infra/elasticjob-infra-common/src/test/java/org/apache/shardingsphere/elasticjob/infra/json/GsonFactoryTest.java
@@ -52,6 +52,19 @@ public final class GsonFactoryTest {
         });
         assertThat(beforeRegisterGson.toJson(new GsonFactoryTest()), is("{}"));
         assertThat(GsonFactory.getGson().toJson(new GsonFactoryTest()), is("test"));
+        GsonFactory.registerTypeAdapter(GsonFactoryTest.class, new TypeAdapter() {
+            
+            @Override
+            public Object read(final JsonReader in) {
+                return null;
+            }
+            
+            @Override
+            public void write(final JsonWriter out, final Object value) throws IOException {
+                out.jsonValue("");
+            }
+        });
+        assertThat(beforeRegisterGson.toJson(new GsonFactoryTest()), is("{}"));
     }
     
     @Test
