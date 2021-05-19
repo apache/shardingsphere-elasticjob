bfcd9d57a192aef673a3a6e0ec4a750e4abf785d
蔡顺铠 <skai.cpp@gmail.com>
2021-05-19 10:11:47 +0800

    roll back the code ，keep the original

--- elasticjob-ecosystem/elasticjob-tracing/elasticjob-tracing-rdb/src/main/java/org/apache/shardingsphere/elasticjob/tracing/rdb/storage/RDBJobEventStorage.java
+++ elasticjob-ecosystem/elasticjob-tracing/elasticjob-tracing-rdb/src/main/java/org/apache/shardingsphere/elasticjob/tracing/rdb/storage/RDBJobEventStorage.java
@@ -358,19 +358,9 @@ public final class RDBJobEventStorage {
             preparedStatement.setString(1, taskId);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
-                    JobStatusTraceEvent jobStatusTraceEvent = new JobStatusTraceEvent(
-                            resultSet.getString(1),
-                            resultSet.getString(2),
-                            resultSet.getString(3),
-                            resultSet.getString(4),
-                            resultSet.getString(5),
-                            Source.valueOf(resultSet.getString(6)),
-                            resultSet.getString(7),
-                            resultSet.getString(8),
-                            State.valueOf(resultSet.getString(9)),
-                            resultSet.getString(10),
-                            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(resultSet.getString(11))
-                    );
+                    JobStatusTraceEvent jobStatusTraceEvent = new JobStatusTraceEvent(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4),
+                            resultSet.getString(5), Source.valueOf(resultSet.getString(6)), resultSet.getString(7), resultSet.getString(8),
+                            State.valueOf(resultSet.getString(9)), resultSet.getString(10), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(resultSet.getString(11)));
                     result.add(jobStatusTraceEvent);
                 }
             }
