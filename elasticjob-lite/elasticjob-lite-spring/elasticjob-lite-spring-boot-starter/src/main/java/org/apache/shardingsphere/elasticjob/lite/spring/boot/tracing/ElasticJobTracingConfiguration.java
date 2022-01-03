f47a801b2f1095b885cbed7be9f0e8a618ad8c3a
cocodroid <2531075716@qq.com>
2022-01-03 23:40:29 +0800

    Add customized Tracing DataSource

--- elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-boot-starter/src/main/java/org/apache/shardingsphere/elasticjob/lite/spring/boot/tracing/ElasticJobTracingConfiguration.java
+++ elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-boot-starter/src/main/java/org/apache/shardingsphere/elasticjob/lite/spring/boot/tracing/ElasticJobTracingConfiguration.java
@@ -17,11 +17,14 @@
 
 package org.apache.shardingsphere.elasticjob.lite.spring.boot.tracing;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.shardingsphere.elasticjob.tracing.api.TracingConfiguration;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.Nullable;
 
 import javax.sql.DataSource;
 
@@ -30,7 +33,18 @@ import javax.sql.DataSource;
  */
 @EnableConfigurationProperties(TracingProperties.class)
 public class ElasticJobTracingConfiguration {
-    
+
+    @Bean("tracingDataSource")
+    public DataSource tracingDataSource(final TracingProperties tracingProperties) {
+        if (tracingProperties.getDataSource() == null) {
+            return null;
+        }
+        HikariDataSource tracingDataSource = new HikariDataSource();
+        tracingDataSource.setJdbcUrl(tracingProperties.getDataSource().getUrl());
+        BeanUtils.copyProperties(tracingProperties.getDataSource(), tracingDataSource);
+        return tracingDataSource;
+    }
+
     /**
      * Create a bean of tracing configuration.
      *
@@ -40,7 +54,11 @@ public class ElasticJobTracingConfiguration {
     @Bean
     @ConditionalOnBean(DataSource.class)
     @ConditionalOnProperty(name = "elasticjob.tracing.type", havingValue = "RDB")
-    public TracingConfiguration<DataSource> tracingConfiguration(final DataSource dataSource) {
-        return new TracingConfiguration<>("RDB", dataSource);
+    public TracingConfiguration<DataSource> tracingConfiguration(final DataSource dataSource, @Nullable DataSource tracingDataSource) {
+        DataSource ds = tracingDataSource;
+        if (ds == null) {
+            ds = dataSource;
+        }
+        return new TracingConfiguration<>("RDB", ds);
     }
 }
