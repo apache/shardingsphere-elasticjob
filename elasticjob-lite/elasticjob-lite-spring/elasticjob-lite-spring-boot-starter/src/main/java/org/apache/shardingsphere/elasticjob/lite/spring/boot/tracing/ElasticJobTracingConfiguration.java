0946af48d24aeb18b9137fc048beae2cf53c2b57
verysu <2531075716@qq.com>
2022-01-06 16:54:46 +0800

    Enhance springboot starter tracing config (#2034)
    * add JobSystemException Constructor
    
    fixed Script Job throw Exception stack was ignored
    
    * Add unit case for JobSystemException
    
    * Add customized Tracing DataSource
    
    * Add custom Tracing DataSource
    
    * Add custom Tracing DataSource: Refactor
    
    * Add custom Tracing DataSource : add param javadoc

--- elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-boot-starter/src/main/java/org/apache/shardingsphere/elasticjob/lite/spring/boot/tracing/ElasticJobTracingConfiguration.java
+++ elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-boot-starter/src/main/java/org/apache/shardingsphere/elasticjob/lite/spring/boot/tracing/ElasticJobTracingConfiguration.java
@@ -17,11 +17,15 @@
 
 package org.apache.shardingsphere.elasticjob.lite.spring.boot.tracing;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.shardingsphere.elasticjob.tracing.api.TracingConfiguration;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.Nullable;
 
 import javax.sql.DataSource;
 
@@ -30,17 +34,40 @@ import javax.sql.DataSource;
  */
 @EnableConfigurationProperties(TracingProperties.class)
 public class ElasticJobTracingConfiguration {
-    
+
+    /**
+     * Create a bean of tracing DataSource.
+     *
+     * @param tracingProperties tracing Properties
+     * @return tracing DataSource
+     */
+    @Bean("tracingDataSource")
+    public DataSource tracingDataSource(final TracingProperties tracingProperties) {
+        DataSourceProperties dataSource = tracingProperties.getDataSource();
+        if (dataSource == null) {
+            return null;
+        }
+        HikariDataSource tracingDataSource = new HikariDataSource();
+        tracingDataSource.setJdbcUrl(dataSource.getUrl());
+        BeanUtils.copyProperties(dataSource, tracingDataSource);
+        return tracingDataSource;
+    }
+
     /**
      * Create a bean of tracing configuration.
      *
      * @param dataSource required by constructor
+     * @param tracingDataSource tracing ataSource
      * @return a bean of tracing configuration
      */
     @Bean
     @ConditionalOnBean(DataSource.class)
     @ConditionalOnProperty(name = "elasticjob.tracing.type", havingValue = "RDB")
-    public TracingConfiguration<DataSource> tracingConfiguration(final DataSource dataSource) {
-        return new TracingConfiguration<>("RDB", dataSource);
+    public TracingConfiguration<DataSource> tracingConfiguration(final DataSource dataSource, @Nullable final DataSource tracingDataSource) {
+        DataSource ds = tracingDataSource;
+        if (ds == null) {
+            ds = dataSource;
+        }
+        return new TracingConfiguration<>("RDB", ds);
     }
 }
