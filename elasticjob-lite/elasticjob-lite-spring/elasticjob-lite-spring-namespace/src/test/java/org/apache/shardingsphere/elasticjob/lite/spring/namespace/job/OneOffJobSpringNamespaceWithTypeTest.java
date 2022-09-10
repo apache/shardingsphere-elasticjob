c210ae9043a9ed0f1f5437f06660c3f281562492
songxiulu <songxiulu@jd.com>
2022-09-10 15:42:50 +0800

    test case: reduce sleep time

--- elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-namespace/src/test/java/org/apache/shardingsphere/elasticjob/lite/spring/namespace/job/OneOffJobSpringNamespaceWithTypeTest.java
+++ elasticjob-lite/elasticjob-lite-spring/elasticjob-lite-spring-namespace/src/test/java/org/apache/shardingsphere/elasticjob/lite/spring/namespace/job/OneOffJobSpringNamespaceWithTypeTest.java
@@ -46,7 +46,7 @@ public final class OneOffJobSpringNamespaceWithTypeTest extends AbstractZookeepe
     public void jobScriptWithJobTypeTest() {
         OneOffJobBootstrap bootstrap = applicationContext.getBean(scriptJobName, OneOffJobBootstrap.class);
         bootstrap.execute();
-        BlockUtils.sleep(2000L);
+        BlockUtils.sleep(1200L);
         assertTrue(regCenter.isExisted("/" + scriptJobName + "/sharding"));
     }
 }
