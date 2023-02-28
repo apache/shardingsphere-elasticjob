19d1580102dba8498105737bb08bdb1c604e3970
wizhuo <46399833+wizhuo@users.noreply.github.com>
2023-02-28 09:31:05 +0800

    Adjust JobScheduler init sequence  to avoid use wrong ElasticJobListener (#2187)
    * Adjust JobScheduler init sequence  to avoid use wrong ElasticJobListener when the config overwrite is false
    
    * adjust code style
    
    * Remove unnecessary methods

--- elasticjob-lite/elasticjob-lite-core/src/test/java/org/apache/shardingsphere/elasticjob/lite/internal/setup/SetUpFacadeTest.java
+++ elasticjob-lite/elasticjob-lite-core/src/test/java/org/apache/shardingsphere/elasticjob/lite/internal/setup/SetUpFacadeTest.java
@@ -17,10 +17,7 @@
 
 package org.apache.shardingsphere.elasticjob.lite.internal.setup;
 
-import org.apache.shardingsphere.elasticjob.api.ElasticJob;
-import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
 import org.apache.shardingsphere.elasticjob.infra.handler.sharding.JobInstance;
-import org.apache.shardingsphere.elasticjob.lite.internal.config.ConfigurationService;
 import org.apache.shardingsphere.elasticjob.lite.internal.election.LeaderService;
 import org.apache.shardingsphere.elasticjob.lite.internal.instance.InstanceService;
 import org.apache.shardingsphere.elasticjob.lite.internal.listener.ListenerManager;
@@ -36,16 +33,11 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class SetUpFacadeTest {
-    
-    @Mock
-    private ConfigurationService configService;
     
     @Mock
     private LeaderService leaderService;
@@ -68,23 +60,13 @@ public final class SetUpFacadeTest {
     public void setUp() {
         JobRegistry.getInstance().addJobInstance("test_job", new JobInstance("127.0.0.1@-@0"));
         setUpFacade = new SetUpFacade(null, "test_job", Collections.emptyList());
-        ReflectionUtils.setFieldValue(setUpFacade, "configService", configService);
         ReflectionUtils.setFieldValue(setUpFacade, "leaderService", leaderService);
         ReflectionUtils.setFieldValue(setUpFacade, "serverService", serverService);
         ReflectionUtils.setFieldValue(setUpFacade, "instanceService", instanceService);
         ReflectionUtils.setFieldValue(setUpFacade, "reconcileService", reconcileService);
         ReflectionUtils.setFieldValue(setUpFacade, "listenerManager", listenerManager);
     }
-    
-    @Test
-    public void assertSetUpJobConfiguration() {
-        JobConfiguration jobConfig = JobConfiguration.newBuilder("test_job", 3)
-                .cron("0/1 * * * * ?").setProperty("streaming.process", Boolean.TRUE.toString()).build();
-        when(configService.setUpJobConfiguration(ElasticJob.class.getName(), jobConfig)).thenReturn(jobConfig);
-        assertThat(setUpFacade.setUpJobConfiguration(ElasticJob.class.getName(), jobConfig), is(jobConfig));
-        verify(configService).setUpJobConfiguration(ElasticJob.class.getName(), jobConfig);
-    }
-    
+
     @Test
     public void assertRegisterStartUpInfo() {
         setUpFacade.registerStartUpInfo(true);
