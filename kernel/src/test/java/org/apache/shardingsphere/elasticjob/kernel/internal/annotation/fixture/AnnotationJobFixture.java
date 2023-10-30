2edfdb60706fe1b755b8bde89fe548bb35618966
zhangliang <zhangliang@apache.org>
2023-10-30 16:47:49 +0800

    Move ecosystem dependencies into bootstrap module

--- kernel/src/test/java/org/apache/shardingsphere/elasticjob/kernel/internal/annotation/fixture/AnnotationJobFixture.java
+++ kernel/src/test/java/org/apache/shardingsphere/elasticjob/kernel/internal/annotation/fixture/AnnotationJobFixture.java
@@ -17,14 +17,12 @@
 
 package org.apache.shardingsphere.elasticjob.kernel.internal.annotation.fixture;
 
-import lombok.Getter;
 import org.apache.shardingsphere.elasticjob.annotation.ElasticJobConfiguration;
 import org.apache.shardingsphere.elasticjob.annotation.ElasticJobProp;
-import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
-import org.apache.shardingsphere.elasticjob.spi.param.ShardingContext;
+import org.apache.shardingsphere.elasticjob.api.ElasticJob;
 
 @ElasticJobConfiguration(
-        jobName = "AnnotationSimpleJob",
+        jobName = "AnnotationJobFixture",
         description = "desc",
         shardingTotalCount = 3,
         shardingItemParameters = "0=a,1=b,2=c",
@@ -33,13 +31,5 @@ import org.apache.shardingsphere.elasticjob.spi.param.ShardingContext;
                 @ElasticJobProp(key = "print.title", value = "test title"),
                 @ElasticJobProp(key = "print.content", value = "test content")
         })
-@Getter
-public class AnnotationJobFixture implements SimpleJob {
-    
-    private volatile boolean completed;
-    
-    @Override
-    public void execute(final ShardingContext shardingContext) {
-        completed = true;
-    }
+public class AnnotationJobFixture implements ElasticJob {
 }
